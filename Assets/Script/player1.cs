a45946ec1a32adad55c61603c61aed1a04c53787
take4588 <takeayudream@gmail.com>
2019-10-15 11:43:42 +0900

    無視リスト追加

---
+++ Assets/Script/player1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class player1 : MonoBehaviour
+{
+    Rigidbody2D rigidPlayer;
+    public float speed = 8.0f;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal1");
+        float y = Input.GetAxisRaw("Vertical1");
+        Vector2 dir = new Vector2(x, y).normalized;
+        GetComponent<Rigidbody2D>().velocity = dir * speed;
+    }
+}
---
+++ Assets/Script/player2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class player2 : MonoBehaviour
+{
+    Rigidbody2D rigidPlayer;
+    public float speed = 8.0f;
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal2");
+        float y = Input.GetAxisRaw("Vertical2");
+        Vector2 dir = new Vector2(x, y).normalized;
+        GetComponent<Rigidbody2D>().velocity = dir * speed;
+    }
+}
