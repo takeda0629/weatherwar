657d8896df6a2972b393035ef5623fabe87774e2
KPC-018A1978\K018A1978 <0bh3162578x480m@ezweb.ne.jp>
2019-10-18 11:56:48 +0900

    Player1のスクリプト変更　ゲームパッド対応の移動とジャンプ

--- Assets/Scripts/player1.cs
+++ Assets/Scripts/player1.cs
@@ -6,19 +6,49 @@ using UnityEngine.UI;
 public class player1 : MonoBehaviour
 {
     Rigidbody2D rigidPlayer;
-    public float speed = 8.0f;
+    [SerializeField] float speed = 8.0f;
+    private Rigidbody2D rb;
+    public bool isGround = false;
+    
 
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
 
     void Update()
     {
+        Move();
+    }
+
+    void Move()
+    {
+        Vector2 velocity = rb.velocity;
+
         float x = Input.GetAxisRaw("Horizontal1");
-        float y = Input.GetAxisRaw("Vertical1");
-        Vector2 dir = new Vector2(x, y).normalized;
-        GetComponent<Rigidbody2D>().velocity = dir * speed;
+        if(Input.GetButtonDown("Jump1") && isGround)
+        {
+            velocity.y = 10;
+            isGround = false;
+        }
+       
+        if(x != 0)
+        {
+            velocity.x = x * speed;
+        }
+
+        rb.velocity = velocity;
+
+
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Floor") )
+        {
+            isGround = true;
+           
+        }
     }
 }
