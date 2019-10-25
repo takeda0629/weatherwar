a0ba2c8409531a935b82948122fa2013310e2cd3
take4588 <takeayudream@gmail.com>
2019-10-25 11:30:25 +0900

    ボタンを押して天候を変化させるイメージ(仮)を作成

---
+++ Assets/ChangeWeather.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ChangeWeather : MonoBehaviour
+{
+    public Sprite sprite1;
+    public Sprite sprite2;
+    private bool chFlg = false;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    public void changeSprite()
+    {
+        this.gameObject.GetComponent<Image>().sprite = sprite2;
+
+        if (!chFlg)
+        {
+            this.gameObject.GetComponent<Image>().sprite = sprite2;
+            chFlg = true;
+        }
+        else
+        {
+            this.gameObject.GetComponent<Image>().sprite = sprite1;
+            chFlg = false;
+        }
+    }
+}
