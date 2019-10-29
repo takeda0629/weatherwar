06606bace53f1888219ceffe4839be5993c7a6d2
KPC-018A1968\K018A1968 <gs15jono@gmail.com>
2019-10-29 14:39:09 +0900

    テスト中

--- Assets/Scripts/player2.cs
+++ Assets/Scripts/player2.cs
@@ -7,16 +7,27 @@ public class player2 : MonoBehaviour
 {
     Rigidbody2D rigidPlayer;
     public float speed = 8.0f;
+
+    public enum playerNo
+    {
+        p1 = 1,
+        p2 = 2,
+        p3 = 3,
+        p4 = 4
+    }
+
+    public playerNo playerno;
     void Start()
     {
-        
+       
     }
 
 
     void Update()
     {
-        float x = Input.GetAxisRaw("Horizontal2");
-        float y = Input.GetAxisRaw("Vertical2");
+        Debug.Log((int)playerno);
+        float x = Input.GetAxisRaw("Horizontal"  +(int)playerno);
+        float y = Input.GetAxisRaw("Vertical1");
         Vector2 dir = new Vector2(x, y).normalized;
         GetComponent<Rigidbody2D>().velocity = dir * speed;
     }
