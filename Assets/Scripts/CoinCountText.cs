a4d4d921864c6c70bca7b91f671e1e31bfd00eca
take4588 <takeayudream@gmail.com>
2019-11-08 12:10:21 +0900

    プレイヤーとコインの当たり判定、プレイヤーのTextの表示

---
+++ Assets/Scripts/CoinCountText.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinCountText : MonoBehaviour
+{
+    public Text countText;
+    public int coinCount = 0;
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        countText.text = coinCount.ToString();
+    }
+
+    public void AddCount(GameObject players)
+    {
+        coinCount += 1;
+    }
+}
