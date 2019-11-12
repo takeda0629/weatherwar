f88891681752b7550633fb5a33de6249ffb92a9c
KPC-018A1968\K018A1968 <gs15jono@gmail.com>
2019-11-12 16:31:32 +0900

    プレイヤーナンバー指定スクリプト

---
+++ Assets/Scripts/PlayerNoSelect.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerNoSelect : MonoBehaviour
+{
+    public enum PlayerNo
+    {
+        p1 = 1,
+        p2 = 2,
+        p3 = 3,
+        p4 = 4
+    }
+
+    [SerializeField] PlayerNo playerNo;
+    public int num;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        num = (int)playerNo;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public int Renum()
+    {
+        return num;
+    }
+
+}
