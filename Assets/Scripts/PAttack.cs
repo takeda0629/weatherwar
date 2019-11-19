aa06107998f04ecdc699deccd50c55e46b102c19
KPC-018A1968\K018A1968 <gs15jono@gmail.com>
2019-11-19 10:39:21 +0900

    攻撃できるようになった

--- Assets/Scripts/PAttack.cs
+++ Assets/Scripts/PAttack.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PAttack : MonoBehaviour
 {
     private Collider2D col2d;
+    private SpriteRenderer spr;
 
     public float attaktime;//判定持続時間
 
@@ -12,23 +13,33 @@ public class PAttack : MonoBehaviour
 
     private bool nowattack;//攻撃中か？
 
+    private int num; //プレイヤー番号取得
+
+    GameObject _parent;
 
     // Start is called before the first frame update
     void Start()
     {
         col2d = this.GetComponent<BoxCollider2D>();
+        spr = this.GetComponent<SpriteRenderer>();
         nowattack = false;
+
+        _parent = transform.root.gameObject;
+        num = _parent.GetComponent<PlayerNoSelect>().num;
+         
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(_parent);
+
         if (nowattack)
         {
             timecount += Time.deltaTime;
         }
 
-        if (Input.GetKeyDown(KeyCode.Space) && !nowattack)
+        if (Input.GetButtonDown("Attack"+num) && !nowattack)
         {
             OnAttack();
         }
@@ -42,16 +53,18 @@ public class PAttack : MonoBehaviour
 
     void OnAttack()
     {
-        Debug.Log("攻撃");
+        //Debug.Log("攻撃");
         col2d.enabled = true;
+        spr.enabled = true;
 
         nowattack = true;
     }
 
     void OffAttack()
     {
-        Debug.Log("攻撃終了");
+        //Debug.Log("攻撃終了");
         col2d.enabled = false;
+        spr.enabled = false;
 
         nowattack = false;
         timecount = 0;
