7ff757407d16ec0112917e469d490dcac783856c
take4588 <takeayudream@gmail.com>
2019-11-19 13:06:47 +0900

    最新版player1

--- Assets/Scripts/player1.cs
+++ Assets/Scripts/player1.cs
@@ -3,22 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class player1 : MonoBehaviour
 {
+    public enum PlayerNo
+    {
+        p1 = 1,
+        p2 = 2,
+        p3 = 3,
+        p4 = 4
+    }
+
     [SerializeField] float speed = 8.0f;
     private Rigidbody2D rb;
     public bool isSelectFlag = false;
-    
+
+
+    bool getcoin = false;
+    public CoinCountText cct;
+    public int counter = 0;
+
+    [SerializeField] PlayerNo playerNo;
+    [SerializeField] float widthRight;
+    [SerializeField] float widthLeft;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        //eventSystem1 = GameObject.Find("1PCount");
+        cct = GameObject.Find((int)playerNo + "PCount").GetComponent<CoinCountText>();
     }
 
 
     void Update()
     {
         Move();
+        FieldLoop();
     }
 
     /// <summary>
@@ -28,10 +48,19 @@ public class player1 : MonoBehaviour
     {
         Vector2 velocity = rb.velocity;
 
-        float x = Input.GetAxisRaw("Horizontal1");
-        float y = Input.GetAxisRaw("Vertical1");
+        float x = Input.GetAxisRaw("Horizontal" + (int)playerNo);
+        float y = Input.GetAxisRaw("Vertical" + (int)playerNo);
         Vector2 dir = new Vector2(x, y).normalized;
         GetComponent<Rigidbody2D>().velocity = dir * speed;
+
+        if (x > 0)  // スティックを右に倒したら
+        {
+            transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);   // 右向き
+        }
+        if (x < 0)  // スティックを左に倒したら
+        {
+            transform.localScale = new Vector3(-1.5f, 1.5f, 1.5f);  // 左向き
+        }
         
     }
 
@@ -56,6 +85,32 @@ public class player1 : MonoBehaviour
         if(col.CompareTag("Character"))
         {
             isSelectFlag = false;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Item")
+        {
+            cct.coinCount += 1;
+            counter += 1;
+        }
+    }
+
+    void FieldLoop()  // 画面端ループ処理
+    {
+        if (rb.transform.position.x > widthRight)
+        {
+            Vector3 rbPos = rb.transform.position;
+            rbPos.x = rbPos.x - 15.5f;
+            rb.transform.position = rbPos;
+        }
+
+        if (rb.transform.position.x < -widthLeft)
+        {
+            Vector3 rbPos = rb.transform.position;
+            rbPos.x = rbPos.x + 15.5f;
+            rb.transform.position = rbPos;
         }
     }
 }
