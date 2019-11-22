b8f0febd026d93ca6bc9c1ad6cea9fa19b5723bf
KPC-018A1978\K018A1978 <0bh3162578x480m@ezweb.ne.jp>
2019-11-22 11:43:25 +0900

    タイトルシーンの作成、セレクトシーンでキャラ選択したらOKと表示するようにした

---
+++ Assets/Scripts/Scene/SceneChanger.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public enum Scenes
+{
+    Title,
+    CharaSelect,
+    GamePlay,
+    Result,
+}
+public class SceneChanger : MonoBehaviour
+{
+    [SerializeField] Scenes next;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        switch(next)
+        {
+            case Scenes.Title:
+                {
+                    SceneManager.LoadScene("Title");
+                }
+                break;
+            case Scenes.CharaSelect:
+                {
+                    SceneManager.LoadScene("Select");
+                }
+                break;
+            case Scenes.GamePlay:
+                {
+                    SceneManager.LoadScene("GamePlay");
+                }
+                break;
+            case Scenes.Result:
+                {
+                    SceneManager.LoadScene("Result");
+                }
+                break;
+        }
+    }
+}
