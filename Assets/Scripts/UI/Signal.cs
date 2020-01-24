304e00763b9ec3f4424664a8ef903179082dfb34
take4588 <takeayudream@gmail.com>
2020-01-24 12:09:12 +0900

    3,2,1,GO！と表示させるUIの追加

---
+++ Assets/Scripts/UI/Signal.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Signal : MonoBehaviour
+{
+    [SerializeField]
+    private Text signalText;
+
+    void Start()
+    {
+        StartCoroutine(Countdown(3));
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Countdown(int seconds)
+    {
+        signalText.text = " 3";
+        yield return new WaitForSeconds(1.0f);
+
+        signalText.text = " 2";
+        yield return new WaitForSeconds(1.0f);
+
+        signalText.text = " 1";
+        yield return new WaitForSeconds(1.0f);
+
+        signalText.text = "GO!";
+        yield return new WaitForSeconds(1.0f);
+
+        signalText.text = "";
+    }
+}
