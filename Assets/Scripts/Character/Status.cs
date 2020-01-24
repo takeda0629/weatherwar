6b0b61e4e516239a0d20153524805495939ea1ed
KPC-018A1978\K018A1978 <0bh3162578x480m@ezweb.ne.jp>
2020-01-24 10:06:30 +0900

    Statusスクリプトをabstract化。Start(),Update(),ChangeStatus()をvirtual化 UpdateにJump()を移植

--- Assets/Scripts/Character/Status.cs
+++ Assets/Scripts/Character/Status.cs
@@ -2,30 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Status : MonoBehaviour
+public abstract class Status : MonoBehaviour
 {
-    ChangeWeather weather;
-    PleyerController pCon;
+    public enum PlayerNo
+    {
+        p1 = 1,
+        p2 = 2,
+        p3 = 3,
+        p4 = 4
+    }
+    protected GameObject _parent;
+    protected int pNum;
 
-    int beforeWeather; //天気格納用変数
-    int nowWeather; //天気格納用変数
-    private float magnification; //倍率
+    protected ChangeWeather weather;
+    protected PleyerController pCon;
 
-    PowerColor powerColor;
+    protected int beforeWeather; //天気格納用変数
+    protected int nowWeather; //天気格納用変数
+    protected float magnification; //倍率
+    protected float jumpP;
 
-    AudioSource audioSource;
-    public AudioClip changeSE;
+    protected PowerColor powerColor;
+
+    protected AudioSource audioSource;
+    [SerializeField] protected AudioClip changeSE;
 
 
     // Start is called before the first frame update
-    void Start()
+    public virtual void Start()
     {
+        _parent = transform.root.gameObject;
+        pNum = _parent.GetComponent<PlayerNoSelect>().num;
+
         weather = GameObject.Find("backG").GetComponent<ChangeWeather>();
         pCon = this.GetComponent<PleyerController>();
 
 
         nowWeather = (int)weather.NowWeather();
-        ChangeStatus();
+        
 
         //powersprite = transform.GetChild(2).GetComponent<GameObject>();
 
@@ -34,8 +48,13 @@ public class Status : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    public virtual void Update()
     {
+        if (Input.GetButtonDown("Jump" + pNum) && pCon.CanJump())
+        {
+            pCon.Jump(jumpP);
+        }
+
         nowWeather = (int)weather.NowWeather();
 
         if (nowWeather != beforeWeather) //天気が変わったら
@@ -51,90 +70,90 @@ public class Status : MonoBehaviour
     /// <summary>
     /// 天候でステータス変化
     /// </summary>
-    public void ChangeStatus()
-    {
-        powerColor = transform.GetChild(2).gameObject.GetComponent<PowerColor>();
-        switch (transform.name)
-        {
-            case "Csun(Clone)":
-                {
-                    if (nowWeather == 0)
-                    {
-                        magnification = 1.1f;
-                        powerColor.SpriteOn();
-                    }
-                    else if (nowWeather == 1)
-                    {
-                       
-                        powerColor.SpriteOff();
-                    }
-                    else
-                    {
-                        magnification = 0.9f;
-                        powerColor.SpriteOff();
-                    }
-                    break;
-                }
-            case "Crai(Clone)":
-                {
-                    if (nowWeather == 1)
-                    {
-                       
-                        powerColor.SpriteOn();
-                    }
-                    else if (nowWeather == 2)
-                    {
-                        
-                        powerColor.SpriteOff();
-                    }
-                    else
-                    {
-                        magnification = 1.0f;
-                        powerColor.SpriteOff();
-                    };
-                    break;
-                }
-            case "Cwin(Clone)":
-                {
-                    if (nowWeather == 2)
-                    {
-                        magnification = 1.5f;
-                        powerColor.SpriteOn();
-                    }
-                    else if (nowWeather == 3)
-                    {
-                        magnification = 0.6f;
-                        powerColor.SpriteOff();
-                    }
-                    else
-                    {
-                        magnification = 1.0f;
-                        powerColor.SpriteOff();
-                    }
-                    break;
-                }
-            case "Csno(Clone)":
-                {
-                    if (nowWeather == 3)
-                    {
-                        magnification = 1.2f;
-                        powerColor.SpriteOn();
-                    }
-                    else if (nowWeather == 0)
-                    {
-                        magnification = 0.8f;
-                        powerColor.SpriteOff();
-                    }
-                    else
-                    {
-                        magnification = 1.0f;
-                        powerColor.SpriteOff();
-                    }
-                    break;
-                }
+    public virtual void ChangeStatus() { }
+    //{
+    //powerColor = transform.GetChild(2).gameObject.GetComponent<PowerColor>();
+    //switch (transform.name)
+    //{
+    //    case "Csun(Clone)":
+    //        {
+    //            if (nowWeather == 0)
+    //            {
+    //                magnification = 1.1f;
+    //                powerColor.SpriteOn();
+    //            }
+    //            else if (nowWeather == 1)
+    //            {
 
-        }
+    //                powerColor.SpriteOff();
+    //            }
+    //            else
+    //            {
+    //                magnification = 0.9f;
+    //                powerColor.SpriteOff();
+    //            }
+    //            break;
+    //        }
+    //    case "Crai(Clone)":
+    //        {
+    //            if (nowWeather == 1)
+    //            {
 
+    //                powerColor.SpriteOn();
+    //            }
+    //            else if (nowWeather == 2)
+    //            {
 
-    }
+    //                powerColor.SpriteOff();
+    //            }
+    //            else
+    //            {
+    //                magnification = 1.0f;
+    //                powerColor.SpriteOff();
+    //            };
+    //            break;
+    //        }
+    //    case "Cwin(Clone)":
+    //        {
+    //            if (nowWeather == 2)
+    //            {
+    //                magnification = 1.5f;
+    //                powerColor.SpriteOn();
+    //            }
+    //            else if (nowWeather == 3)
+    //            {
+    //                magnification = 0.6f;
+    //                powerColor.SpriteOff();
+    //            }
+    //            else
+    //            {
+    //                magnification = 1.0f;
+    //                powerColor.SpriteOff();
+    //            }
+    //            break;
+    //        }
+    //    case "Csno(Clone)":
+    //        {
+    //            if (nowWeather == 3)
+    //            {
+    //                magnification = 1.2f;
+    //                powerColor.SpriteOn();
+    //            }
+    //            else if (nowWeather == 0)
+    //            {
+    //                magnification = 0.8f;
+    //                powerColor.SpriteOff();
+    //            }
+    //            else
+    //            {
+    //                magnification = 1.0f;
+    //                powerColor.SpriteOff();
+    //            }
+    //            break;
+    //        }
+
+    //}
+
+    //}
+
 }
