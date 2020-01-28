c3ce1c7babe0a3032465d62cb05079abb3f7ec9d
KPC-018A1978\K018A1978 <0bh3162578x480m@ezweb.ne.jp>
2020-01-28 09:21:06 +0900

    使ってないので関係なし

--- Assets/Scripts/player/player1.cs
+++ Assets/Scripts/player/player1.cs
@@ -94,7 +94,7 @@ public class player1 : MonoBehaviour
         if (col.gameObject.tag == "Item")
         {
             Debug.Log("get");
-            cct.AddCount();
+            cct.AddCount(1);
             counter += 1;
         }
     }
