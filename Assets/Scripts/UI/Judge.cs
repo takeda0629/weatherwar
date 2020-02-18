c91d314f53ecd07d7728fbf67974810525a94509
KPC-018A1968\K018A1968 <gs15jono@gmail.com>
2020-02-18 16:18:48 +0900

    勝利バッチの表示ひな形

--- Assets/Scripts/UI/Judge.cs
+++ Assets/Scripts/UI/Judge.cs
@@ -20,10 +20,13 @@ public class Judge : MonoBehaviour
 
     bool once = true;
 
+    public GameObject crown;
+
     // Start is called before the first frame update
     void Start()
     {
-       
+        crown = GetComponent<GameObject>();
+        crown.transform.position = new Vector3(0, 0, 1);
     }
 
     // Update is called once per frame
@@ -48,22 +51,31 @@ public class Judge : MonoBehaviour
     {
         if(p1Coin > p2Coin && p1Coin > p3Coin && p1Coin > p4Coin)
         {
-            wineerText.text = "Player1の勝ち!!";
+            Vector3  pos = new Vector3(-6.32f, 0.5f, 1f);
+            crown.transform.position = pos;
+            wineerText.text = "Player1 の勝ち!!";
         }
         else if(p2Coin > p1Coin && p2Coin > p3Coin && p2Coin > p4Coin)
         {
-            wineerText.text = "Player2の勝ち!!";
+            Vector3 pos = new Vector3(-2.7f, 0.5f, 1f);
+            crown.transform.position = pos;
+            wineerText.text = "Player2 の勝ち!!";
         }
         else if(p3Coin > p1Coin && p3Coin > p2Coin && p3Coin > p4Coin)
         {
-            wineerText.text = "Player3の勝ち!!";
+            Vector3 pos = new Vector3(2.1f, 0.5f, 1f);
+            crown.transform.position = pos;
+            wineerText.text = "Player3 の勝ち!!";
         }
         else if(p4Coin > p1Coin && p4Coin > p2Coin && p4Coin > p3Coin)
         {
-            wineerText.text = "Player4の勝ち!!";
+            Vector3 pos = new Vector3(6.57f, 0.5f, 1f);
+            crown.transform.position = pos;
+            wineerText.text = "Player4 の勝ち!!";
         }
         else
         {
+            crown.SetActive(false);
             wineerText.text = "1位が誰だっていいじゃないか。人間だもの。\n ウェザ男";
         }
 
